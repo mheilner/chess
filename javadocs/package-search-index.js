70b17328ae8cd56a273a02ece269cf82ef6e8d9c
mheilner <mark.heilner4@gmail.com>
2023-10-14 21:22:22 -0600

    adding javadocs

---
+++ javadocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chess"},{"l":"chessPkg"},{"l":"dataAccess"},{"l":"model"},{"l":"requests"},{"l":"results"},{"l":"services"}];updateSearchResults();
