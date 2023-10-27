98fe5c5dab705145b4f6b834e5c37460816065d5
mheilner <mark.heilner4@gmail.com>
2023-10-27 10:53:38 -0600

    added clear test

---
+++ src/test/unitTests/ClearTest.java
@@ -0,0 +1,24 @@
+package unitTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import services.ClearService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ClearTest {
+
+    private ClearService clearService;
+
+    @BeforeEach
+    public void setUp() {
+        clearService = new ClearService();
+    }
+
+    @Test
+    @DisplayName("Test successful clearing of database")
+    public void testClearDatabaseSuccess() {
+        assertTrue(clearService.clear(), "Failed to clear the database.");
+    }
+}
